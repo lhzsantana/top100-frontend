7dbc4981eea27ed5de8522e5d3f2c381e5f1598d
Luiz Santana <luiz.santana@socialbase.com.br>
2015-11-05 11:53:51 -0200

    Initial commit

--- app/controllers/feedController.js
+++ app/controllers/feedController.js
@@ -1,39 +1,51 @@
-
 angular.module("myApp").controller("FeedController", [
-	'$scope','$location',
-    function($scope, $location){
+    '$scope', '$location',
+    function ($scope, $location ) {
 
-    $scope.republicas=[
-       {id:1, name:'Republica com nome curto', description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'},
-       {id:2, name:'Republica com nome looooooooooooooooooooooongo', description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'}
-     ];
+        $scope.republicas = [
+            {
+                id: 1,
+                name: 'Republica com nome curto',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+            },
+            {
+                id: 2,
+                name: 'Republica com nome looooooooooooooooooooooongo',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+            }
+        ];
 
 
-       $scope.news = [
-              {id:1, title: 'Nova republica', description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'},
-              {id:2, title: 'Novo estudante da sua univeridade', description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'}
-            ];
+        $scope.news = [
+            {
+                id: 1,
+                title: 'Nova republica',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+            },
+            {
+                id: 2,
+                title: 'Novo estudante da sua univeridade',
+                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+            }
+        ];
 
-        $scope.loadRepublicas = function(){
-
-            return $scope.republicas;
-        },
-
-        $scope.loadNews = function(){
+        $scope.loadNews = function () {
             return $scope.news;
-        },
+        };
 
-        $scope.like = function(){
+        $scope.like = function () {
+            console.log("Like");
+        }
 
-           console.log("Like");
+        $scope.share = function () {
+            $scope.news.unshift({title: "Nova ideia", description: $scope.user.idea});
+            console.log($scope.user.idea);
+        }
+
+        $scope.showModalNewRepublica = function () {
+            console.log("show modal");
 
         }
 
-        $scope.share = function(){
-
-         $scope.news.unshift({title:"Nova ideia",description:$scope.user.idea});
-           console.log($scope.user.idea);
-
-        }
     }
 ]);
---
+++ app/controllers/studentController.js
@@ -0,0 +1,34 @@
+angular.module("myApp").controller("StudentController", [
+    '$scope', '$location',
+    function ($scope, $location) {
+
+        $scope.currentStudent = {
+            name:'Luiz',
+            university:'UNESP',
+            degree:'Ciência da Computação',
+            city:'São José do Rio Preto'
+        }
+
+        $scope.students = [
+
+        ];
+
+        $scope.newStudent = function () {
+
+            $scope.students.unshift(
+                {
+                    name: $scope.student.name,
+                    university: $scope.student.university,
+                    degree: $scope.student.degree,
+                    city: $scope.student.city
+                }
+            );
+
+        };
+
+        $scope.loadStudents = function () {
+
+            return $scope.students;
+        };
+    }
+]);
