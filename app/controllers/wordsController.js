3b2d41ad3f97bc3ed376c8024f238b5206db906d
Luiz Santana <luiz.santana@socialbase.com.br>
2016-01-08 14:43:29 -0200

    Improved words presentation

--- app/controllers/wordsController.js
+++ app/controllers/wordsController.js
@@ -1,8 +1,6 @@
 angular.module("myApp").controller("WordsController", [
     '$scope', '$http',
-    function ($scope, $http) {
-
-        $scope.logged=false;
+    function ($scope, $rootScope, $http) {
 
         $scope.$on('searchEvent', function(event, args) {
 
@@ -14,37 +12,73 @@ angular.module("myApp").controller("WordsController", [
             {
                 id: 1,
                 title: 'Nova republica',
-                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+                description: 'Lorem Ipsum'
             },
             {
                 id: 2,
-                title: 'Novo estudante da sua univeridade',
-                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
-            },
-            {
-                id: 3,
-                title: 'Novo estudante da sua univeridade',
-                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
-            },
-            {
-                id: 3,
-                title: 'Novo estudante da sua univeridade',
-                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
-            },
-            {
-                id: 3,
-                title: 'Novo estudante da sua univeridade',
-                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
-            },
-            {
-                id: 3,
-                title: 'Novo estudante da sua univeridade',
-                description: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+                title: 'Nova republica',
+                description: 'Lorem Ipsum'
             }
         ];
 
+        $scope.days = [
+            {
+                date:"06/01/2016",
+                words: $scope.words
+            },
+            {
+                date:"07/01/2016",
+                words: $scope.words
+            },
+            {
+                date:"08/01/2016",
+                words: $scope.words
+            },
+        ];
+
+
         $scope.load = function () {
-            return $scope.words;
+
+            return $scope.days;
+
+            /*
+            $http
+                .get("http://www.omdbapi.com/?t=")
+                .success(
+                function(response){$scope.details = response;});
+            */
+        };
+
+
+        $scope.getCurrentLanguage = function () {
+
+            if(!$rootScope.currentLanguage){
+                $rootScope.currentLanguage="English";
+            }
+
+            return $rootScope.currentLanguage;
+        }
+
+        $scope.changeLanguage = function (language) {
+
+            $rootScope.currentLanguage="abc";
+            switch(language) {
+                case "EN":
+                    $rootScope.currentLanguage="English";
+                    break;
+                case "PT":
+                    $rootScope.currentLanguage="Português";
+                    break;
+                case "ES":
+                    $rootScope.currentLanguage="Español";
+                    break;
+                case "DE":
+                    $rootScope.currentLanguage="Deutsch";
+                    break;
+                case "FR":
+                    $rootScope.currentLanguage="Français";
+                    break;
+            }
         };
 
     }
